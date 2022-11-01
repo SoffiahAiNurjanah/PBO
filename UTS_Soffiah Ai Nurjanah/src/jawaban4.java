a0253a98255d006fbe9e190e3919caf25698395a
SoffiahAiNurjanah <113439781+SoffiahAiNurjanah@users.noreply.github.com>
2022-11-01 14:55:10 +0700

    Add files via upload

---
+++ UTS_Soffiah Ai Nurjanah/src/jawaban4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author SOFFIAH AI NURJANAH
+ */
+public class jawaban4 {
+    public static void main(String[] args) {
+        
+        for (int j=2; j <= 100; j += 2) {
+            System.out.println("Perulangan genap" +j);
+        }
+    }
+}
